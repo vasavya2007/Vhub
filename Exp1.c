08b6abc1b44992e1ef40a6fe97ab04d6b7ad7cd3
VASAVYA POTHIREDDY <pothireddy.vasavya@s.amity.edu>
2025-02-12 14:38:19 +0530

    Multiplication

--- Exp1.c
+++ Exp1.c
@@ -3,6 +3,8 @@ void main(){
 	int a = 30;
 	int b =20;
 	printf("The sum of the numbers is:%d", a+b);
-        printf("THe difference is:%d", a-b);
+	printf("THe difference is:%d", a-b);
+	printf("the product is:%d", a/b);
+
 
 }
