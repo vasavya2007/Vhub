ad09d36b394e32d18a486ced49202e3cd31304b9
VASAVYA POTHIREDDY <pothireddy.vasavya@s.amity.edu>
2025-02-12 14:28:11 +0530

    Added the substraction code

--- Exp1.c
+++ Exp1.c
@@ -2,6 +2,7 @@
 void main(){
 	int a = 30;
 	int b =20;
-	printf("Them sum of the numbers is:%d", a+b);
+	printf("The sum of the numbers is:%d", a+b);
+        printf("THe difference is:%d", a-b);
 
 }
